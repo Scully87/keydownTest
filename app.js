cec89690f4f7c07cd03a1601741a2894821f90cf
Scully87 <scully87.ts@googlemail.com>
2015-02-04 11:07:28 +0000

    mario moving, up down left right

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $(document).keydown(function(key){}); 
-  
+    $(document).keydown(function(key) {
+
+        switch(parseInt(key.which,10)) {
+
+      // Left Arrow Pressed
+      case 37:
+          $('#mario').animate({left: "-=15px"}, '1000');
+        break;    
+
+      // Up Arrow Pressed
+      case 38:
+          $('#mario').animate({top: "-=15px"}, '1000');
+        break;
+            
+      // Right Arrow Pressed
+      case 39:
+          $('#mario').animate({left: "+=15px"}, '1000');
+        break;
+
+      // Down Arrow Pressed
+      case 40:
+          $('#mario').animate({top: "+=15px"}, '1000');
+        break;
+    }    
+
+  });
+
 });
